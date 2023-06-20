e678fb54283d3de2511f286489bdf4f48f826d78
Mugheera <mugheeram567@gmail.com>
2023-06-20 15:23:54 -0400

    complete math operations using command line

---
+++ math_operations.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+all_args =sys.argv
+args = all_args[1:]
+
+first_value = int(args[0])
+second_value = int(args[1])
+
+if args[2] == "--addition":
+    sum = first_value + second_value
+    print("sum is",sum)
+
+elif args[2] == "--subtraction":
+    subtraction = first_value - second_value
+    print("Difference is",subtraction)
+
+elif args[2] == "--division":
+    division= second_value / first_value
+    print("quotient is",division)
+
+elif args[2] == "--multiplication":
+    multiplication= first_value * second_value
+    print("Product is",multiplication)
+
+else:
+    raise Exception("Invalid operation\n valid operations are  [--addition,--subtraction,--division,--multiplication]")
+
+
